5bdf07c754bcf5b94c21376833d4900e151eb92f
igor <igalvesreis2121@gmail.com>
2025-06-01 16:48:31 -0300

    gh-page ok

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: '/pizzaria-model',
+  assetPrefix: '/pizzaria-model/',
 };
 
 export default nextConfig;
